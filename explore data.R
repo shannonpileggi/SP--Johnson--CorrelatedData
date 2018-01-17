8a936ed53833d5c32b4163ef03d0a51b42462ac7
Emily Johnson <emilyjohnson@MacBook-Air.local>
2018-01-16 20:35:49 -0800

    created new R script to input and work with data file

---
+++ explore data.R
@@ -0,0 +1,12 @@
+data <- read.csv("/Users/emilyjohnson/Desktop/Stats_Senior_Project_Data.csv", header = TRUE)
+head(data)
+data_sub <- data[c(1:3,19)]
+head(data_sub)
+tapply(data_sub$tfamtml, as.factor(data_sub$bottype), mean, na.rm = TRUE)
+## botttype = 1 --> clear bottle
+## botttype = 2 --> opaque bottle
+## mean for clear bottle = 105.44579 mL
+## mean for opaque bottle = 96.24353 mL
+tapply(data_sub$tfamtml, as.factor(data_sub$bottype), sd, na.rm = TRUE)
+## sd for clear bottle = 52.43197 mL
+## sd for opaque bottle = 44.76531 mL
