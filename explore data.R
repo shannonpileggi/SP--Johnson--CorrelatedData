411183af97ea437f7497e305f8f4c54c506ba609
Emily Johnson <emilyjohnson@MacBook-Air.local>
2018-01-23 21:51:55 -0800

    more work in R and updating hours sheet

--- explore data.R
+++ explore data.R
@@ -12,22 +12,57 @@ tapply(data_sub$tfamtml, as.factor(data_sub$bottype), sd, na.rm = TRUE)
 ## sd for opaque bottle = 44.76531 mL
 
 data_sub <- data.frame(data_sub)
+data_sub <- na.omit(data_sub)
+data_sub <- data_sub[-109,]
 
 for(i in 1:nrow(data_sub)){
   
   if((data_sub$VISIT[i] == 1 & data_sub$bottype[i] == 1) | 
      (data_sub$VISIT[i] == 2 & data_sub$bottype[i] == 2)){
-    data_sub$type[i] == 'A'
+    data_sub$type[i] <- 'A'
   } else {
-    data_sub$type[i] == 'B'
+    data_sub$type[i] <- 'B'
   }
   
 }
 
-data_sub
+# Group A: clear bottle on 1st visit, opaque bottle on 2nd visit
+groupA <- subset(data_sub, type == 'A')
+groupA_clear <- subset(groupA, bottype == 1)
+groupA_opaque <- subset(groupA, bottype == 2)
+groupA <- cbind(groupA_clear, groupA_opaque$tfamtml)
+groupA <- subset(groupA[,c(1,4,6)])
+names(groupA) <- c("Study ID", "Clear Bottle mL", "Opaque Bottle mL")
+groupA$Difference <- groupA$`Clear Bottle mL` - groupA$`Opaque Bottle mL`
+mean(groupA$`Clear Bottle mL`)
+mean(groupA$`Opaque Bottle mL`)
+mean(groupA$Difference)
+sd(groupA$`Clear Bottle mL`)
+sd(groupA$`Opaque Bottle mL`)
+# mean difference = 8.879838
+# mean clear bottle = 98.37078 mL
+# mean opaque bottle = 89.49094 mL
+# sd clear bottle = 53.70171 mL
+# sd opaque bottle = 44.66042 mL
+t.test(groupA$`Clear Bottle mL`, groupA$`Opaque Bottle mL`, paired = TRUE)
 
-if(identical(data_sub$VISIT[1], 1)) {
-  print ("YES")
-} else {
-  print ("NO")
-}
+# Group B: opaue bottle on 1st visit, clear bottle on 2nd visit
+groupB <- subset(data_sub, type == 'B')
+groupB_clear <- subset(groupB, bottype == 1)
+groupB_opaque <- subset(groupB, bottype == 2)
+groupB <- cbind(groupB_clear, groupB_opaque$tfamtml)
+groupB <- subset(groupB[,c(1,4,6)])
+names(groupB) <- c("Study ID", "Clear Bottle mL", "Opaque Bottle mL")
+groupB$Difference <- groupB$`Clear Bottle mL` - groupB$`Opaque Bottle mL`
+mean(groupB$Difference)
+mean(groupB$`Clear Bottle mL`)
+mean(groupB$`Opaque Bottle mL`)
+sd(groupB$`Clear Bottle mL`)
+sd(groupB$`Opaque Bottle mL`)
+# mean difference = 6.974264
+# mean clear bottle = 109.7927
+# mean opaque bottle = 102.8184
+# sd clear bottle = 49.44655
+# sd opaque bottle = 44.46599
+t.test(groupB$`Clear Bottle mL`, groupB$`Opaque Bottle mL`, paired = TRUE)
+
